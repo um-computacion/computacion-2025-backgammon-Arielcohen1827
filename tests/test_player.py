89372869afc7516bbd1dc0d12ed81782500e8b73
arielcohen1827 <arieleiancohen@gmail.com>
2025-09-17 22:24:03 -0300

    test de player

--- tests/test_player.py
+++ tests/test_player.py
@@ -1,3 +1,5 @@
+# tests/test_player.py
+
 import unittest
 from core.player import Player
 
@@ -15,6 +17,30 @@ class TestPlayer(unittest.TestCase):
     def test_get_ficha(self):
         self.assertEqual(self.player_x.get_ficha(), "X")
         self.assertEqual(self.player_o.get_ficha(), "O")
+
+    def test_roll_dice_returns_two_values(self):
+        rolls = self.player_x.roll_dice()
+        self.assertEqual(len(rolls), 2)
+        self.assertTrue(1 <= rolls[0] <= 6)
+        self.assertTrue(1 <= rolls[1] <= 6)
+
+    def test_get_last_roll_matches_roll_dice(self):
+        rolls = self.player_x.roll_dice()
+        last_rolls = self.player_x.get_last_roll()
+        self.assertEqual(rolls, last_rolls)
+
+    def test_has_double_true_when_both_dice_same(self):
+        # Forzamos los valores manualmente para simular un doble
+        self.player_x._Player__dice__.last_rolls = [3, 3]
+        self.assertTrue(self.player_x.has_double())
+
+    def test_has_double_false_when_dice_different(self):
+        self.player_x._Player__dice__.last_rolls = [2, 5]
+        self.assertFalse(self.player_x.has_double())
+
+    def test_str_representation(self):
+        self.assertEqual(str(self.player_x), "Jugador Ariel (X)")
+        self.assertEqual(str(self.player_o), "Jugador CPU (O)")
 
 
 if __name__ == "__main__":
